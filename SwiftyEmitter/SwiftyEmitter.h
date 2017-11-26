baa29a7de76c01a9f7b83d1de96b8cce59a6b96f
yuta24 <yuta24@bivre.com>
2017-11-26 19:37:26 +0900

    Create project

---
+++ SwiftyEmitter/SwiftyEmitter.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyEmitter.h
+//  SwiftyEmitter
+//
+//  Created by yuta24 on 2017/11/26.
+//  Copyright © 2017 yuta24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyEmitter.
+FOUNDATION_EXPORT double SwiftyEmitterVersionNumber;
+
+//! Project version string for SwiftyEmitter.
+FOUNDATION_EXPORT const unsigned char SwiftyEmitterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyEmitter/PublicHeader.h>
+
+
